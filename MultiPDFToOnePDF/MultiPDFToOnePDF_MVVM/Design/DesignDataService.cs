c3c716e5e6cea23451007f28aba7f5c8296cf8a3
Manish <Manish@DESKTOP-85IMROD>
2017-01-31 00:05:27 +0530

    Created to project ,1 simple WPF and Other MVVM Date 31-01-2017(12:05AM)

---
+++ MultiPDFToOnePDF/MultiPDFToOnePDF_MVVM/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MultiPDFToOnePDF_MVVM.Model;
+
+namespace MultiPDFToOnePDF_MVVM.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
